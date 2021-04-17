eb052d038e9d04d82e8958a297af3843c6c39ccc
fraziercs2i <jgwenet@hotmail.com>
2021-04-18 01:25:41 +0200

    Initial commit

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,97 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+@Tag("controllers")
+class IndexControllerTest {
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new IndexController();
+    }
+    @DisplayName("Test proper View name is for index page")
+    @Test
+    void index() {
+        assertEquals("index", controller.index());
+        assertEquals("index", controller.index(), "Wrong View Returned");
+
+        assertEquals("index", controller.index(), () -> "Another Expensive Message " +
+                "Make me only if you have to");
+    }
+
+    @Test
+    @DisplayName("Test Exception")
+    void oupsHandler() {
+            // assertTrue("notimplemented".equals(controller.oopsHandler()), () -> "This is some expensive " +
+            //  "Message to build" +
+            // "for my test");
+        assertThrows(ValueNotFoundException.class, () -> {
+            controller.oopsHandler();
+
+        });
+    }
+
+    @Disabled("Demo of timeout")
+    @Test
+    void TestTimeOut() {
+        assertTimeout(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+            System.out.println("I got here");
+        });
+    }
+    @Disabled
+    @Test
+    void testTimeOutprempt(){
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+            System.out.println("I got here 2342342342342");
+        });
+    }
+    @Test
+    void testAssumptionTrue() {
+        assumeTrue("GURU".equalsIgnoreCase(System.getenv("GURU_RUNTIME")));
+    }
+    @Test
+    void testAssumptionTrueAssumptionIsTrue() {
+        assumeTrue("GURU".equalsIgnoreCase("GURU"));
+    }
+    @EnabledOnOs(OS.MAC)
+    @Test
+    void testMeOnMacOS() {
+
+    }
+    @EnabledOnOs(OS.WINDOWS)
+    @Test
+    void testMeOnWindows() {
+    }
+
+    @EnabledOnJre(JRE.JAVA_8)
+    @Test
+    void testMeOnJava8() {
+
+    }
+    @EnabledOnJre(JRE.JAVA_11)
+    @Test
+    void testMeOnJava11() {
+
+    }
+    @EnabledIfEnvironmentVariable(named = "USER", matches = "jt")
+    @Test
+    void testIfUserJT() {
+
+    }
+    @EnabledIfEnvironmentVariable(named="USER",matches="fred")
+    @Test
+    void testIfUserFred() {
+    }
+
+
+}
